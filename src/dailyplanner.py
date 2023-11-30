9d0ee88b4e8e57286754c68eb69cb0103fab62d4
Owen Kaiser <ock230000@utdallas.edu>
2023-11-30 15:46:37 -0600

    added base url, and started response

--- src/dailyplanner.py
+++ src/dailyplanner.py
@@ -1,9 +1,18 @@
 import requests
 
 def get_weather(api_key, city):
-    base_url = ""
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city,
         "appid": api_key,
         "units": "imperial"
     }
+
+     response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        print(f"Error: Unable to fetch weather data. Status code: {response.status_code}")
+        return None
